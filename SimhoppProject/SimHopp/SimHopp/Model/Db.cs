bc28c4c331f78318aa27ea367f30a64a4e4c2d79
cyrusthegreat90 <35834434+cyrusthegreat90@users.noreply.github.com>
2018-02-07 15:12:05 +0100

    Version 1

---
+++ SimhoppProject/SimHopp/SimHopp/Model/Db.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SimHopp.Model
+{
+    class Db
+    {
+        void loginDB()
+        {
+            try
+            {
+                SqlConnectionStringBuilder connString = new SqlConnectionStringBuilder()
+                {
+                    DataSource = "simhoppdb.database.windows.net",
+                    UserID = "SimHopp",
+                    Password = "10helluworlD",
+                    InitialCatalog = "SIMHOPP"
+                };
+                using (SqlConnection connection = new SqlConnection(connString.ConnectionString))
+                {
+                    connection.Open();
+                    string Query = "SELECT * FROM [LoginProfile] p";
+
+                    using (SqlCommand cmd = new SqlCommand(Query, connection))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+
+                            }
+                        }
+
+                    }
+                }
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
